820ce8103a242a0498a1b6607b73591bfe17fa8a
Mikhail-Kreytser <mikhail.kreytser@gmail.com>
2019-11-13 00:27:39 -0500

    resolved comments

--- openSpotServer/parkingSpot/DEMO_ParkingSpotMask.py
+++ openSpotServer/parkingSpot/DEMO_ParkingSpotMask.py
@@ -1,15 +1,23 @@
 import cv2
-from parkingSpots import *
-from parkingSpotMask import get_mask_of_parking_spot
+import parkingSpots
+from parkingSpotMask import ( get_mask_image_of_parking_spot, get_mask_array_of_parking_spot)
 
 # 0 Flag stands for grayscale.
+#	image = cv2.imread('parkingLot.jpg', 0)
 # Shape of grayscale is (height, width)
-# Shape of color is (height, width, [r, g, b])
+# Shape of color is (height, width, [b, g, r])
 # Use gray scale makes generating the 2D mask array easier
-image = cv2.imread('parkingLot.jpg', 0)
+image = cv2.imread('parkingLot.jpg')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+																				 # Defult : white
+image = get_mask_image_of_parking_spot(image, parkingSpots.spot_15, (0, 0, 255)) # Red
+image = get_mask_image_of_parking_spot(image, parkingSpots.spot_16, (0, 0, 255)) # Red
+image = get_mask_image_of_parking_spot(image, parkingSpots.spot_17, (0, 255, 0)) # Green
 
-image = get_mask_of_parking_spot(image, spot_16, True)
-image = get_mask_of_parking_spot(image, spot_17, True)
-image = get_mask_of_parking_spot(image, spot_15, True)
+cv2.imwrite('maskedParkingLotOverlay.jpg', image)
 
-cv2.imwrite('maskedParkingLot.jpg', image)
+																			  # Defult : 1 (shows up as dark grey on image)
+image = get_mask_array_of_parking_spot(gray_image, parkingSpots.spot_15, 255) # 255 shows up as white on image 
+
+cv2.imwrite('maskedParkingSpot.jpg', image)
+
