68d314a02335f5b95aefd4e1683cdf7b5483dbff
khelif96 <khelif96@gmail.com>
2019-12-19 01:07:41 -0500

    fix code styling

--- detection/frontend/src/components/index.js
+++ detection/frontend/src/components/index.js
@@ -1,5 +1,8 @@
 import SpotCount from './spotCount/SpotCount';
 import Selector from './selector/Selector';
 import Image from './image/Image';
-import SpotList from './spotList/SpotList'
-export { SpotCount, Selector, Image, SpotList };
+import SpotList from './spotList/SpotList';
+
+export {
+  SpotCount, Selector, Image, SpotList,
+};
